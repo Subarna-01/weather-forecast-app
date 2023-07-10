06491ae106384f0b42cb98d4ea63936b0d7ccec7
Subarna Bhowmik <104106066+Subarna-01@users.noreply.github.com>
2023-07-10 21:55:49 +0530

    Update urls.py

--- weatherforecast/weatherforecast/urls.py
+++ weatherforecast/weatherforecast/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("client.urls")),
+    path("weatherforecast/",include("client.urls")),
 ]
 urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
